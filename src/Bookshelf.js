aea55d6adf0b708240d7a54f5a3531e0420721c7
Felix Li <felix@FelixPro.local>
2020-05-08 15:25:48 +0800

    refine bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -2,14 +2,14 @@ import React from "react";
 import Book from "./Book";
 
 function Bookshelf(props) {
-  const { books, shelfName } = props;
+  const { books, shelf } = props;
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">{shelfName}</h2>
+      <h2 className="bookshelf-title">{shelf.name}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {books.map((book) => (
-            <Book book={book} />
+            <Book book={book} key={book.id} />
           ))}
         </ol>
       </div>
